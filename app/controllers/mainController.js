d0fcecd577afda9a0f548195e8b73c12cf44c3e2
KaioFernandes <kaio.fernandesdev@gmail.com>
2021-09-03 10:50:39 +0200

    need a push

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -3,7 +3,7 @@ const dataMapper = require('../dataMapper');
 module.exports = {
 
     initVar: (_, response, next) => {
-        response.locals.pageTitle = 'Pokedex, attrapez-les tous !!'; response.locals.pageDescription = `Un pokédex, est une sorte de dictionnaire de tous les pokémon (petites créatures fictives et adorables). Ces derniers peuvent se battre
+        response.locals.pageTitle = 'Pokedex, attrapez-les tous !!'; response.locals.pageDescription = `Un pokédex, est une sorte de dictionnaire de tous les pokémon . Ces derniers peuvent se battre
         et disposent de caractéristiques de combat appelées statistiques. Chaque pokémon possède aussi un ou deux types (plante, roche, feu...).`;
         next();
     },
